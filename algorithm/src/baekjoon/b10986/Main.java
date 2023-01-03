d4ab21d7c5814b26c7fa4fc8ad20e4e052a54c68
Seph-23 <seongmlyu95@gmail.com>
2023-01-03 15:56:50 +0900

    feat: 추가, 나머지 합 구하기

---
+++ algorithm/src/baekjoon/b10986/Main.java
@@ -0,0 +1,35 @@
+package baekjoon.b10986;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int no = sc.nextInt();
+    int divisor = sc.nextInt();
+    long[] preSum = new long[no];
+    long[] C = new long[divisor];
+    long answer = 0;
+
+    //구간 합 배열 만들기
+    preSum[0] = sc.nextInt();
+    for (int i = 1; i < no; i++) {
+      preSum[i] = preSum[i - 1] + sc.nextInt();
+    }
+
+    for (int i = 0; i < no; i++) {    //합 배열의 모든 값에 % 연산 수행하기
+      int remainder = (int) (preSum[i] % divisor);
+      if (remainder == 0) {           //0 ~ i 까지의 구간 합 자체가 0일 때 정답에 더하기
+        answer++;
+      }
+      C[remainder]++;                 //나머지가 같은 인덱스의 개수 카운팅하기
+    }
+
+    for (int i = 0; i < divisor; i++) {
+      if (C[i] > 1) {
+        answer = answer + (C[i] * (C[i] - 1) / 2);    //나머지가 같은 인덱스 중 2개를 뽑는 경우의 수를 더하기
+      }
+    }
+    System.out.println(answer);
+  }
+}
